d1d889cee5b57b3f64e7e5612667c555f2208b07
BuildTools <unconfigured@null.spigotmc.org>
2019-08-25 17:15:49 -0400

    Added Command Role

--- main.js
+++ main.js
@@ -14,7 +14,21 @@ client.on("message", msg => {
   if (cmd === `${PREFIX}ping`) msg.reply("Pong!");
   if (cmd === `${PREFIX}say`) {
     msg.channel.send(args.join(" "));
-    msg.delete().then(msg => console.log(`Message Delete`));
+    msg.delete().then(console.log(`Message Delete`));
+  }
+  if (cmd === `${PREFIX}role`) {
+    const channel = client.channels.find(r => r.name === "ecorte-logs");
+    const role = msg.guild.roles.find(r => r.name === args[0]);
+    if (!role) return msg.channel.send("This role does not exist");
+    if (msg.member.roles.find(r => r.name === args[0])) {
+      msg.member.roles.remove(role);
+      msg.channel.send(`Role: ${role} removed from ${msg.author}.`);
+      channel.send(`Role: ${role} removed from ${msg.author}.`);
+    } else {
+      msg.member.roles.add(role);
+      msg.channel.send(`Role: ${role} added to ${msg.author}.`);
+      channel.send(`Role: ${role} added to ${msg.author}.`);
+    }
   }
 });
 
