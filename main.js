de69963d78352cb36936fa664fc78b7bf2ed4dda
BuildTools <unconfigured@null.spigotmc.org>
2019-08-25 16:00:06 -0400

    v3

--- main.js
+++ main.js
@@ -2,12 +2,20 @@ const Discord = require("discord.js");
 const { TOKEN, PREFIX } = require("./config");
 const client = new Discord.Client({ disableEveryone: true });
 
-client.on("ready", () => {
-  console.log("Ready");
-});
+client.on("ready", () => console.log("Ready"));
+client.on("error", console.error);
+client.on("warn", console.warn);
+client.on("debug", console.log);
 
 client.on("message", msg => {
-  if (msg.content.startsWith(`${PREFIX}ping`)) msg.reply("Pong!");
+  if (msg.author.bot) return;
+  const args = msg.content.split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+  if (cmd === `${PREFIX}ping`) msg.reply("Pong!");
+  if (cmd === `${PREFIX}say`) {
+    msg.channel.send(args.join(" "));
+    msg.delete().then(msg => console.log(`Message Delete`));
+  }
 });
 
 client.login(TOKEN);
