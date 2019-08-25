282efe3ee3dd24f8fd994361615990d57e46ed4b
BuildTools <unconfigured@null.spigotmc.org>
2019-08-25 09:27:55 -0400

    v2

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 const Discord = require("discord.js");
+const { TOKEN, PREFIX } = require("./config");
 const client = new Discord.Client({ disableEveryone: true });
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log("Ready");
 });
 
 client.on("message", msg => {
-  if (msg.content === "ping") msg.reply("Pong!");
-  if (msg.content === "everyone") msg.channel.send("@everyone , Salut");
-  if (msg.content === "noteveryone")
-    msg.channel.send("@everyone (noteveryone) , Salut");
+  if (msg.content.startsWith(`${PREFIX}ping`)) msg.reply("Pong!");
 });
 
-client.login("NjEzMDQwODM1Njg0MDczNTA2.XWGkdA.es76brx0Ky0lk9e8bVbFgGRhr0c");
+client.login(TOKEN);
