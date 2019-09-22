ea12bc51903f9670362915635d5228873bb06e0f
BuildTools <unconfigured@null.spigotmc.org>
2019-09-22 12:00:48 -0400

    ts

--- events/guildMemberAdd.js
+++
@@ -1,5 +0,0 @@
-module.exports = (client, member) => {
-  member.send("Welcome!");
-  const channel = client.channel.find(r => r.name === "general");
-  channel.send(`${member} Welcome to ther server`);
-};
